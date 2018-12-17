77303790321a22ecab4d57628ede3036fcf6b116
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-12-17 11:06:41 +0530

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,1 +1,59 @@
 
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class A
+{
+	void fun1()
+	{
+		System.out.println("fun1 from class A is called");
+	}
+	void fun2()
+	{
+		System.out.println("fun2 from class A is called");
+	}
+	void print()
+	{
+		System.out.println("print method from class A is called(overridden method)");
+	}
+}
+class B extends A
+{
+	void fun3()
+	{
+		System.out.println("fun3 from class B is called");
+	}
+	void fun4()
+	{
+		System.out.println("fun4 from class B is called");
+	}
+	void print()
+	{
+		System.out.println("print method from class B is called(overridden method)");
+	}
+}
+class C extends B
+{
+	void fun5()
+	{
+		System.out.println("fun5 from class C is called");
+	}
+	void fun6()
+	{
+		System.out.println("fun6 from class C is called");
+	}
+	void print()
+	{
+		System.out.println("print method from class C is called(overridden method)");
+	}
+}
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	}
+}
